4f952dc828eb564059945110fa7e05578e4dce5b
Khang Dang <khangdang0199@gmail.com>
2025-01-20 23:03:42 -0500

    Data types

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,9 +35,80 @@ public class App {
 //
 //        System.out.println(employee.toString());
 
-        SampleClass sampleClass = new SampleClass();
-        sampleClass.foo();
-        String name = sampleClass.name;
-        System.out.println(name);
+//        SampleClass sampleClass = new SampleClass();
+//        sampleClass.foo();
+//        String name = sampleClass.name;
+//        System.out.println(name);
+        Product product = new Product();
+        System.out.println(product);
+
+    }
+    private static class Product {
+        private int id;
+        private String name;
+        private BigDecimal price;
+        private float quantity;
+        private float weight;
+        private double amount;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public BigDecimal getPrice() {
+            return price;
+        }
+
+        public void setPrice(BigDecimal price) {
+            this.price = price;
+        }
+
+        public float getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(float quantity) {
+            this.quantity = quantity;
+        }
+
+        public float getWeight() {
+            return weight;
+        }
+
+        public void setWeight(float weight) {
+            this.weight = weight;
+        }
+
+        public double getAmount() {
+            return amount;
+        }
+
+        public void setAmount(double amount) {
+            this.amount = amount;
+        }
+
+        @Override
+        public String toString() {
+            return "Product{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", price=" + price +
+                    ", quantity=" + quantity +
+                    ", weight=" + weight +
+                    ", amount=" + amount +
+                    '}';
+        }
     }
 }
